b3fb4299b6fefbaedf03d9e5d512451f3f43f0f3
hby <3206391486@qq.com>
2024-01-31 15:45:46 +0800

    sixth commit

---
+++ tool.h
@@ -0,0 +1,25 @@
+#ifndef tool_h_
+#define tool_h_
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif
